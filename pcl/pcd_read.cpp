157dfe926095b5caa08cd54f0019771bc9949b86
vu gia truong <gachiemchiep@gmail.com>
2019-09-08 11:21:11 +0900

    Add

---
+++ pcl/pcd_read.cpp
@@ -0,0 +1,14 @@
+#include <pcl/io/pcd_io.h>
+
+int
+main(int argc, char** argv)
+{
+	// Object for storing the point cloud.
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	// Read a PCD file from disk.
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) != 0)
+	{
+		return -1;
+	}
+}
